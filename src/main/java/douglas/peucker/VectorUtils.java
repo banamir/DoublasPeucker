64ed845be39050fc8f6ba32371e1524b4f4f5f10
Miron Banyas <miron.banyas@gmail.com>
2016-06-24 13:20:04 +0300

    Added the algorithms of finding  an intersection of segments

--- src/main/java/douglas/peucker/VectorUtils.java
+++ src/main/java/douglas/peucker/VectorUtils.java
@@ -4,7 +4,7 @@ import java.awt.geom.Point2D;
 import static java.lang.Math.*;
 
 
-public class VectorUtils {
+public final class VectorUtils {
 
     public static double distanseToSigment(Point2D p, Point2D start, Point2D end){
         Point2D r  = vectDif(end, start),
@@ -19,6 +19,44 @@ public class VectorUtils {
                (r1_2 >= r_2 + r2_2)? Math.sqrt(r2_2) :
                        Math.abs(vectProduct(r,r1)/sqrt(r_2));
     }
+
+    public static boolean intersect(Point2D a, Point2D b, Point2D c, Point2D d){
+        Point2D ab = vectDif(a,b), ac = vectDif(a,c), ad = vectDif(a,d);
+        Point2D cd = vectDif(c, d), ca = vectDif(c, a), cb = vectDif(c, b);
+        return intersect(a.getX(),b.getX(),c.getX(),d.getX()) &&
+                vectProduct(ab,ac) *  vectProduct(ab,ad) <=0  &&
+                vectProduct(cd,ca) *  vectProduct(cd,cb) <=0;
+
+    }
+
+    public static boolean intersect(double a, double b, double c, double d) {
+        double p;
+        if(a > b) { p = a; a = b; b = p; }
+        if(c > d) { p = c; c = d; d = p;  }
+        return max(a,c) <= min(b,d);
+    }
+
+    public static Point2D intersectPoint(Point2D a, Point2D b, Point2D c, Point2D d) {
+        if(!intersect(a,b,c,d)) return  null;
+
+        if(Double.compare(a.getX(),b.getX()) == 0 && Double.compare(a.getY(),b.getY()) == 0 )
+            return a;
+        if(Double.compare(c.getX(),d.getX()) == 0 && Double.compare(c.getY(),d.getY()) == 0 )
+            return c;
+
+        double A1 = b.getY() - a.getY(), A2 = d.getY() - c.getY(),
+               B1 = a.getX() - b.getX(), B2 = c.getX() - d.getX(),
+               C1 = A1 * a.getX() + B1 * a.getY(),
+               C2 = A2 * c.getX() + B2 * c.getY(),
+               D  = A1 * B2 - B1 * A2,
+               D1 = C1 * B2 - B1 * C2,
+               D2 = A1 * C2 - C1 * A2;
+        if(Double.compare(D,0.) == 0) return null;
+
+        return new Point2D.Double(D1/D,D2/D);
+    }
+
+
 
     public static Point2D vectDif(Point2D a, Point2D b){
         return new Point2D.Double(a.getX()-b.getX(), a.getY()-b.getY());
@@ -39,4 +77,5 @@ public class VectorUtils {
     public static double vectProduct(Point2D a, Point2D b) {
         return a.getX()*b.getY() - a.getY()*b.getX();
     }
+
 }
--- src/test/java/douglas/peucker/VectorUtilsTest.java
+++ src/test/java/douglas/peucker/VectorUtilsTest.java
@@ -1,16 +1,21 @@
 package douglas.peucker;
 
 import static douglas.peucker.VectorUtils.*;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.awt.*;
 import java.awt.geom.Point2D;
 
 
 
 public class VectorUtilsTest  {
+
+    public static void assertPoint2DEquals(Point2D expected, Point2D actual, double delta) {
+        assertEquals(expected.getX(),actual.getX(), delta);
+        assertEquals(expected.getY(),actual.getY(), delta);
+    }
 
     private static final double DELTA = 1e-15;
 
@@ -201,5 +206,142 @@ public class VectorUtilsTest  {
         a = new Point2D.Double(0.7, 0.3);
         b = new Point2D.Double(0.5, 0.1);
         assertEquals(-0.08,vectProduct(a, b),DELTA);
+    }
+
+    @Test
+    public void testIntersect() throws Exception {
+        Point2D a, b, c ,d;
+
+        a = new Point2D.Double(1,1); b = new Point2D.Double(5,5);
+
+        c = new Point2D.Double(2,2); d = new Point2D.Double(2,2);
+        assertTrue(intersect(a,b,c,d));
+
+        c = new Point2D.Double(4,2); d = new Point2D.Double(7,3);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(1,7); d = new Point2D.Double(2,4);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(9,8); d = new Point2D.Double(7,4);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(5,7); d = new Point2D.Double(5,7);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(6,6); d = new Point2D.Double(3,3);
+        assertTrue(intersect(a, b, c, d));
+
+        c = new Point2D.Double(3,5); d = new Point2D.Double(5,3);
+        assertTrue(intersect(a, b, c, d));
+
+        c = new Point2D.Double(5,7); d = new Point2D.Double(5,7);
+        assertFalse(intersect(a, b, c, d));
+
+
+        c = new Point2D.Double(0,0); d = new Point2D.Double(5,0);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(7,0); d = new Point2D.Double(9,0);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(7,2); d = new Point2D.Double(7,4);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(0,2); d = new Point2D.Double(0,6);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(2,3); d = new Point2D.Double(2,5);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(7,1); d = new Point2D.Double(9,1);
+        assertFalse(intersect(a, b, c, d));
+
+        c = new Point2D.Double(3,6); d = new Point2D.Double(3,2);
+        assertTrue(intersect(a, b, c, d));
+
+        c = new Point2D.Double(4,4); d = new Point2D.Double(6,4);
+        assertTrue(intersect(a, b, c, d));
+
+    }
+
+    @Test
+    public void testIntersect1() throws Exception {
+
+        double a = 0., b = 2., c = 4., d = 6.;
+
+        assertFalse(intersect(a, b, c, d));
+        assertFalse(intersect(c, d, a, b));
+        assertFalse(intersect(b, a, d, c));
+        assertTrue(intersect(a, c, b, d));
+        assertTrue(intersect(a, d, b, c));
+        assertTrue(intersect(b, c, a, d));
+        assertTrue(intersect(b,c,c,d));
+        assertTrue(intersect(b,c,d,c));
+    }
+
+    @Test
+    public void testIntersectPoint() throws Exception {
+        Point2D a, b, c ,d,p;
+
+        a = new Point2D.Double(1,1); b = new Point2D.Double(5,5);
+
+        c = new Point2D.Double(2,2); d = new Point2D.Double(2,2);
+        assertNotNull(p = intersectPoint(a,b,c,d));
+        assertEquals(2., p.getX(), DELTA);
+        assertEquals(2., p.getY(), DELTA);
+
+        c = new Point2D.Double(4,2); d = new Point2D.Double(7,3);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(1,7); d = new Point2D.Double(2,4);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(9,8); d = new Point2D.Double(7,4);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(5,7); d = new Point2D.Double(5,7);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(6,6); d = new Point2D.Double(3,3);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(3,5); d = new Point2D.Double(5,3);
+        assertNotNull(p = intersectPoint(a, b, c, d));
+        assertEquals(4., p.getX(), DELTA);
+        assertEquals(4., p.getY(), DELTA);
+
+
+        c = new Point2D.Double(5,7); d = new Point2D.Double(5,7);
+        assertNull(intersectPoint(a, b, c, d));
+
+
+        c = new Point2D.Double(0,0); d = new Point2D.Double(5,0);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(7,0); d = new Point2D.Double(9,0);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(7,2); d = new Point2D.Double(7,4);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(0,2); d = new Point2D.Double(0,6);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(2,3); d = new Point2D.Double(2,5);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(7,1); d = new Point2D.Double(9,1);
+        assertNull(intersectPoint(a, b, c, d));
+
+        c = new Point2D.Double(3,6); d = new Point2D.Double(3,2);
+        assertNotNull(p = intersectPoint(a, b, c, d));
+        assertEquals(3., p.getX(),DELTA);
+        assertEquals(3., p.getY(), DELTA);
+
+        c = new Point2D.Double(4,4); d = new Point2D.Double(6,4);
+        assertNotNull(p = intersectPoint(a, b, c, d));
+        assertEquals(4., p.getX(),DELTA);
+        assertEquals(4., p.getY(), DELTA);
     }
 }
